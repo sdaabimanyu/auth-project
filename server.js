0c3346acced91969b765e2a6319b95c4cb2a6cd2
sdaabimanyu <sdaabimanyu@gmail.com>
2026-04-25 07:47:16 +0530

    update default port to 5000 in .env and server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 connectDB();
 
 // safe port fallback
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
